cb21226ad5113090a5d29c0852e0bdc13d9f9bb9
Virginia Davenport <virginialdport@gmail.com>
2020-07-15 16:03:36 -0600

    MVP

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -19,7 +19,13 @@ function findById(id){
 }
 
 function findSteps(id){
-    
+    return db("schemes")
+        
+        .join( "steps", "schemes.id", "steps.scheme_id" )
+        .select("schemes.scheme_name", "steps.step_number", "steps.instructions")
+        .where({scheme_id:id})
+        .orderBy("steps.step_number")
+        
 }
 
 function add(scheme){
